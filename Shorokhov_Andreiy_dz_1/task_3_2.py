ae618dcfad289da785c7b70ceab02d31cae3fe66
Shorokhov_A <andr2409@bk.ru>
2021-03-05 11:56:46 +0300

    modify task_3_2.py

---
+++ Shorokhov_Andreiy_dz_1/task_3_2.py
@@ -0,0 +1,23 @@
+WORDS = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'}
+
+
+def num_translate(text):
+
+    if text.istitle():
+
+        return WORDS.get(text.lower()).capitalize()
+
+    return WORDS.get(text)
+
+
+print(num_translate('seven'))
